65fef681ea3f731e6b69fd73c6d116095216f667
PolGuixe <pol@zetoff.com>
2017-04-20 13:26:45 +0200

    added a connected container

---
+++ ReactNativeApp/src/config/config.js
@@ -0,0 +1,4 @@
+export default {
+  // SERVER_URL: 'ws://localhost:3000/websocket',
+  SERVER_URL: 'ws://192.168.251.219:3000/websocket',
+};
--- ReactNativeApp/src/index.js
+++ ReactNativeApp/src/index.js
@@ -1,35 +1,8 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
+import Meteor, { createContainer } from 'react-native-meteor';
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import config from './config/config';
 
-export default class ReactNativeApp extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
 
 const styles = StyleSheet.create({
   container: {
@@ -49,3 +22,32 @@ const styles = StyleSheet.create({
     marginBottom: 5,
   },
 });
+
+Meteor.connect(config.SERVER_URL);
+
+class ReactNativeApp extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.ios.js
+        </Text>
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload,{'\n'}
+          Cmd+D or shake for dev menu
+        </Text>
+      </View>
+    );
+  }
+}
+
+const ConnectedApp = createContainer(() => {
+  const user = Meteor.user();
+
+  return { user };
+}, ReactNativeApp);
+
+export default ConnectedApp;
