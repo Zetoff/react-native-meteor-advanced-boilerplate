5b147c2d4d66abfc62446653ff0cc4c77b529b91
PolGuixe <pol@zetoff.com>
2017-04-25 20:20:23 +0200

    updated primary color

--- ReactNativeApp/src/config/colors.js
+++ ReactNativeApp/src/config/colors.js
@@ -1,8 +1,8 @@
 export default {
-  primary: '#ce28d1',
+  primary: '#6d28d1',
   background: '#f7f7f7',
   defaultText: '#181818',
-  alternateText: '#ce28d1',
+  alternateText: '#6d28d1',
   iconSubtle: '#a2a2a2',
   textSubtle: '#848484',
   red: '#F44336',
